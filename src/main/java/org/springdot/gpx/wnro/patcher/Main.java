e8c30aa745ab75940eb668985a64c6f6606aa031
mspring <mspring@cisco.com>
2020-01-20 12:00:59 -0800

    empty initial project

---
+++ src/main/java/org/springdot/gpx/wnro/patcher/Main.java
@@ -0,0 +1,8 @@
+package org.springdot.gpx.wnro.patcher;
+
+public class Main{
+
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
---
+++ src/test/java/org/springdot/gpx/wnro/patcher/MyTest.java
@@ -0,0 +1,11 @@
+package org.springdot.gpx.wnro.patcher;
+
+import org.junit.Test;
+
+public class MyTest{
+
+    @Test
+    public void testIt() throws Exception{
+        Main.main(null);
+    }
+}
